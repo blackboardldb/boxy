afd5b31f539ab89b874fc0f2efc22332a3199acb
bsheepla <b.sheep.la@gmail.com>
2026-04-19 19:22:35 -0400

    chore: Purga masiva de 13 archivos basura. Eliminados scripts obsoletos y rutas dummys (Sentry, Auth) protegiendo sync-auth-users.ts

--- tmp_inspect.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('app/admin/alumnos/[id]/page.tsx', 'utf8');
-const lines = content.split('\n');
-const imports = lines.filter(l => l.startsWith('import '));
-console.log(imports.join('\n'));
